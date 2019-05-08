b1aead21d75a2b0c8b6042dc3b3b7efb48c99700
raisprilude <rais.prilude@gmail.com>
2019-05-08 22:17:32 +0700

    Ieu Nu INITIAL

---
+++ app/src/main/java/com/afn/afnapp/activity/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.afn.afnapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.WindowDecorActionBar;
+import android.view.View;
+import android.view.Window;
+
+import com.afn.afnapp.R;
+
+import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
+import static android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
