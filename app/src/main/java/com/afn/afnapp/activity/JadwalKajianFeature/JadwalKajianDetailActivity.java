f421052d4438b894cc9d7e0e40f1dde8cde0a8e4
sweetrice <raiszainuri09@gmail.com>
2019-09-30 22:24:50 +0000

    Jadwal Kajian

---
+++ app/src/main/java/com/afn/afnapp/activity/JadwalKajianFeature/JadwalKajianDetailActivity.java
@@ -0,0 +1,15 @@
+package com.afn.afnapp.activity.JadwalKajianFeature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.afn.afnapp.R;
+
+public class JadwalKajianDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal_kajian_detail);
+    }
+}
--- app/src/main/java/com/afn/afnapp/activity/JadwalKajianFeature/JadwalKajianMainActivity.java
+++ app/src/main/java/com/afn/afnapp/activity/JadwalKajianFeature/JadwalKajianMainActivity.java
@@ -1,14 +1,19 @@
 package com.afn.afnapp.activity.JadwalKajianFeature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.afn.afnapp.R;
 import com.afn.afnapp.adapter.JadwalKajianAdapter;
 import com.afn.afnapp.model.JadwalKajianModel;
+import com.afn.afnapp.model.JadwalSholat.Jadwal;
 import com.github.paolorotolo.expandableheightlistview.ExpandableHeightGridView;
 
 import java.util.ArrayList;
@@ -42,6 +47,21 @@ public class JadwalKajianMainActivity extends AppCompatActivity {
     }
 
     void initClick() {
+        gvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                JadwalKajianModel mm;
+                if (etSearch.getText().length() > 0) {
+                    mm = listKajian2.get(i);
+                } else {
+                    mm = listKajian.get(i);
+                }
+
+                Toast.makeText(JadwalKajianMainActivity.this, mm.getJudulKajian() + "", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(JadwalKajianMainActivity.this, JadwalKajianDetailActivity.class));
+            }
+        });
+
         etSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
